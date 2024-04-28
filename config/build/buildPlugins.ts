87fa79125105f3919a327f193c241c66c9d8638e
AidarAx <akhmetgaliev_aydar@mail.ru>
2024-04-28 18:57:36 +0300

    add HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,4 @@
-import { WebpackPluginInstance, ProgressPlugin, DefinePlugin } from 'webpack'
+import { WebpackPluginInstance, ProgressPlugin, DefinePlugin, HotModuleReplacementPlugin } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './type/config';
@@ -15,6 +15,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstanc
     }),
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
+    new HotModuleReplacementPlugin()
   ]
 }
