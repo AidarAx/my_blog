a2b92e65916ee091760706f0e85e994ef1901ee3
AidarAx <akhmetgaliev_aydar@mail.ru>
2024-05-10 11:57:16 +0300

    fix

--- config/jest/identity-obj-proxy/identity-obj-proxy-esm.ts
+++ config/jest/identity-obj-proxy/identity-obj-proxy-esm.ts
@@ -4,9 +4,9 @@ module.exports = new Proxy(
     get: function getter(target, key) {
       if (key === '__esModule') {
         // True instead of false to pretend we're an ES module.
-        return true;
+        return true
       }
-      return key;
-    },
-  },
-);
+      return key
+    }
+  }
+)
