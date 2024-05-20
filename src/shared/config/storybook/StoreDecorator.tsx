13d0591cb05aa7d15a77947681a85808476af573
AidarAx <akhmetgaliev_aydar@mail.ru>
2024-05-20 22:30:06 +0300

    fix

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -1,7 +1,7 @@
 import { StoryFn } from '@storybook/react'
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 
-export const StoreDecorator = (store: Partial<StateSchema>) => (Story: StoryFn) => {
+export const StoreDecorator = (store?: Partial<StateSchema>) => (Story: StoryFn) => {
   if (store) {
     return (
       <StoreProvider initialState={store}>
