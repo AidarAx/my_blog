2e8386af91cd77dff2b950b868409b3930d5a139
AidarAx <akhmetgaliev_aydar@mail.ru>
2024-06-05 21:51:43 +0300

    add new theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -11,7 +11,20 @@ export function useTheme(): useThemeResult {
   const { theme, setTheme } = useContext(ThemeContext)
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
+    let newTheme: Theme
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT
+        break
+      case Theme.LIGHT:
+        newTheme = Theme.PINK
+        break
+      case Theme.PINK:
+        newTheme = Theme.DARK
+        break
+      default:
+        newTheme = Theme.LIGHT
+    }
     setTheme?.(newTheme)
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
   }
